581b54b049c43dc30e3b2801ffecb702d66ba4eb
Pablo Embarach <pabloembarach@gmail.com>
2023-03-01 18:47:44 +0000

    Create profiles app. Add to installed app. Create model for user profiles. Provide acess to Profiles model in admin panel.

---
+++ profiles/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../IMG_0038_lm65ne'
+    )
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)
