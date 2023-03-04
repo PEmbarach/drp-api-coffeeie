6a12db0687bf269ffcf2d98126959896b4cc30bc
Pablo Embarach <pabloembarach@gmail.com>
2023-03-04 17:48:13 +0000

    added a search field by location to Details views.

--- details/views.py
+++ details/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Details
 from .serializers import DetailsSerializer
@@ -8,6 +9,15 @@ class DetailsList(generics.ListCreateAPIView):
     serializer_class = DetailsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Details.objects.all()
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    search_fields = [
+        'owner__username',
+        'location',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
